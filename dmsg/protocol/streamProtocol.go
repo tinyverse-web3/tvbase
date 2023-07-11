e0122780e251264da35787f826df67fa1d16c8f3
jacky <softwarecheng@gmail.com>
2023-07-11 20:01:07 +0800

    u

--- dmsg/protocol/streamProtocol.go
+++ dmsg/protocol/streamProtocol.go
@@ -2,7 +2,6 @@ package protocol
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -12,7 +11,6 @@ import (
 )
 
 func SendProtocolMsg(ctx context.Context, id peer.ID, p protocol.ID, data proto.Message, host host.Host) error {
-	fmt.Printf("/n###SendProtocolMsg###:%v/n", data)
 	stream, err := host.NewStream(ctx, id, p)
 	if err != nil {
 		return err
