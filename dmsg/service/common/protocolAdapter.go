d99eda56321eb49a54990ee24e04b8eb1addfc43
jacky <softwarecheng@gmail.com>
2023-07-11 15:51:41 +0800

    update

--- dmsg/service/common/protocolAdapter.go
+++ dmsg/service/common/protocolAdapter.go
@@ -10,49 +10,49 @@ type CommonProtocolAdapter struct {
 }
 
 func (adapter *CommonProtocolAdapter) GetResponseProtocolID() pb.ProtocolID {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->GetRequestProtocolID: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->GetRequestProtocolID: not implemented")
 	return -1
 }
 
 func (adapter *CommonProtocolAdapter) GetStreamResponseProtocolID() protocol.ID {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->GetStreamResponseProtocolID: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->GetStreamResponseProtocolID: not implemented")
 	return protocol.ID("noimplement")
 }
 
 func (adapter *CommonProtocolAdapter) SetProtocolResponseFailRet(errMsg string) {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->SetProtocolResponseFailRet: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->SetProtocolResponseFailRet: not implemented")
 }
 
 func (adapter *CommonProtocolAdapter) SetProtocolResponseRet(code int32, result string) {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->SetProtocolResponseRet: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->SetProtocolResponseRet: not implemented")
 }
 
 func (adapter *CommonProtocolAdapter) GetProtocolRequestBasicData() *pb.BasicData {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->GetProtocolRequestBasicData: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->GetProtocolRequestBasicData: not implemented")
 	return nil
 }
 
 func (adapter *CommonProtocolAdapter) GetProtocolResponseBasicData() *pb.BasicData {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->GetProtocolResponseBasicData: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->GetProtocolResponseBasicData: not implemented")
 	return nil
 }
 
 func (adapter *CommonProtocolAdapter) InitProtocolResponse(basicData *pb.BasicData, data interface{}) error {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->InitProtocolResponse: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->InitProtocolResponse: not implemented")
 	return nil
 }
 
 func (adapter *CommonProtocolAdapter) SetProtocolResponseSign(signature []byte) error {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->SetProtocolResponseSign: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->SetProtocolResponseSign: not implemented")
 	return nil
 }
 
 func (adapter *CommonProtocolAdapter) CallProtocolRequestCallback() (interface{}, error) {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->CallProtocolRequestCallback: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->CallProtocolRequestCallback: not implemented")
 	return nil, nil
 }
 
 func (adapter *CommonProtocolAdapter) CallProtocolResponseCallback() (interface{}, error) {
-	dmsgLog.Logger.Warnf("CommonProtocolAdapter->CallProtocolResponseCallback: not implemented")
+	dmsgLog.Logger.Debugf("CommonProtocolAdapter->CallProtocolResponseCallback: not implemented")
 	return nil, nil
 }
