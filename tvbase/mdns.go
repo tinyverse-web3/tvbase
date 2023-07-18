66904d24259ec0d2bcbe9eed420edbe86cf93160
jamescjm <chenjianming@tinyverse.space>
2023-07-18 14:20:12 +0800

    modified:   tvbase/mdns.go

--- tvbase/mdns.go
+++ tvbase/mdns.go
@@ -2,6 +2,7 @@ package tvbase
 
 import (
 	"context"
+	"strings"
 
 	"github.com/libp2p/go-libp2p/core/host"
 	libp2pPeer "github.com/libp2p/go-libp2p/core/peer"
@@ -39,7 +40,7 @@ func (m *TvBase) initMdns(ph host.Host, lc fx.Lifecycle) (mdns.Service, error) {
 	lc.Append(fx.Hook{
 		OnStart: func(_ context.Context) error {
 			err := mdnsService.Start()
-			if err != nil {
+			if err != nil && !strings.Contains(err.Error(), "netlinkrib") {
 				tvLog.Logger.Errorf("tvBase->initMdns: mdns start error: %v", err)
 				return err
 			}
