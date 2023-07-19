2314234d6923515db41dbc76eb0661d66adf7a9d
jacky <softwarecheng@gmail.com>
2023-07-19 13:12:57 +0800

    fix

--- tvbase/peer.go
+++ tvbase/peer.go
@@ -37,8 +37,6 @@ func (m *TvBase) registPeerInfo(peerID libp2pPeer.ID) {
 	if result.Error != nil {
 		tvLog.Logger.Errorf("tvBase->registPeerInfo: try get peer info: %v happen error, result: %v", peerID, result)
 		return
-	} else {
-		// tvLog.Logger.Infof("tvBase->registPeerInfo: try get peer info: %v, result: %v", peerID, result)
 	}
 
 	if result.NodeInfo == nil {
