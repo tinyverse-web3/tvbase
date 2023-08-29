a0937687bd1167b806b084376131ee86b3856a39
jacky <softwarecheng@gmail.com>
2023-08-29 14:22:54 +0800

    fix

--- tvnode/boot.sh
+++ tvnode/boot.sh
@@ -14,8 +14,6 @@ else
     exit -1
 fi
 
-# ./tvnode
-echo "Start tvnode..."
 
 user_dir="~"
 user_dir=$(eval echo "$user_dir")
@@ -24,6 +22,7 @@ pid_file="$user_dir/.tvnode/tvnode.pid"
 isKillOldPid=0
 if [ -f "$pid_file" ]; then
     pid=$(cat "$pid_file")
+    echo "killing tvnode, pid: $pid"
     if [ -z "$pid" ]; then
         echo "PID is empty"
     else
@@ -40,27 +39,28 @@ if [ -f "$pid_file" ]; then
     fi
 fi
 
-if [ "$isKillOldPid" -eq 0 ]; then
-    echo "Try to killall for tvnode"
-    killall tvnode
-    kill_result=$?
-    if [ $kill_result -eq 0 ]; then
-        echo "Process killall successfully"
-    else
-        echo "Failed to killall for tvnode (error code: $kill_result)"
-    fi
-fi
-
-
-# log_dir="$user_dir/.tvnode"
-# log_prefix="tvnode"
-# log_filename="$log_dir/$(date +"%Y-%m-%d_%H-%M-%S")_$log_prefix.log"
-# nohup tvnode > "$log_filename" 2>&1 &
-# pid=$!
-# if [ $? -eq 0 ]; then
-#     echo "tvnode is started, write to $pid_file, pid: $pid."
-#     echo "$pid" > $pid_file
-# else
-#     cat $log_filename
-#     echo "tvnode execution failed. check $log_filename for details."
+# if [ "$isKillOldPid" -eq 0 ]; then
+#     echo "Try to killall for tvnode"
+#     killall tvnode
+#     kill_result=$?
+#     if [ $kill_result -eq 0 ]; then
+#         echo "Process killall successfully"
+#     else
+#         echo "Failed to killall for tvnode (error code: $kill_result)"
+#     fi
 # fi
+
+echo "start tvnode..."
+
+log_dir="$user_dir/.tvnode"
+log_prefix="tvnode"
+log_filename="$log_dir/$(date +"%Y-%m-%d_%H-%M-%S")_$log_prefix.log"
+nohup tvnode > "$log_filename" 2>&1 &
+pid=$!
+if [ $? -eq 0 ]; then
+    echo "tvnode is started, write to $pid_file, pid: $pid."
+    echo "$pid" > $pid_file
+else
+    cat $log_filename
+    echo "tvnode execution failed. check $log_filename for details."
+fi
