9e857a4619de050587cabd9eb32032932ec8b720
jacky <softwarecheng@gmail.com>
2023-09-11 15:58:09 +0800

    fix

--- dmsg/protocol/custom/ipfs/define.go
+++ dmsg/protocol/custom/ipfs/define.go
@@ -12,7 +12,7 @@ const SYNCIPFSFILEPID = "tv_syncipfsfile"
 
 const (
 	CODE_PIN            = 0
-	CODE_ALREADY_PIN    = 0
+	CODE_ALREADY_PIN    = 1
 	CODE_ERROR_IPFS     = -1
 	CODE_ERROR_PROTOCOL = -2
 )
