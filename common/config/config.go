264201af859a83b8818acdd5b324f497936c3d40
jacky <softwarecheng@gmail.com>
2023-09-11 18:54:22 +0800

    fix

--- common/config/config.go
+++ common/config/config.go
@@ -244,7 +244,7 @@ func NewDefaultNodeConfig(mode define.NodeMode) *NodeConfig {
 		},
 		CustomProtocol: CustomProtocolConfig{
 			IpfsSyncFile: IpfsSyncFileConfig{
-				IpfsURL: "/ip4/0.0.0.0/tcp/5001",
+				IpfsURL: "/ip4/127.0.0.1/tcp/5001",
 			},
 		},
 		Relay: RelayConfig{
